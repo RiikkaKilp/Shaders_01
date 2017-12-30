573bcb6e5cfd2e3faf8b7f89602cd4bd9c00a93d
RiikkaKilp <RiikkaKilp@users.noreply.github.com>
2017-12-30 18:50:49 +0200

    RandomBullshittery
    2D platformer stuff, not shaders. Demoing moving platforms

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+
+    Vector3 startPos;
+
+    [Range(2.0f, -2.0f)]
+    public float timingOffset = 1;
+
+    public float y_Movement;
+
+	void Start ()
+    {
+        startPos = transform.position;
+	}
+	
+	void FixedUpdate ()
+    {
+        transform.position = startPos + new Vector3(0, Mathf.Sin(Time.time) * y_Movement * timingOffset, 0);
+	}
+}
---
+++ Assets/Scripts/MovingPlatform1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform1 : MonoBehaviour {
+
+
+    Vector3 startPos;
+
+	void Start ()
+    {
+        startPos = transform.position;
+	}
+	
+	void FixedUpdate ()
+    {
+        transform.position = startPos + new Vector3(0, Mathf.Sin(Time.time) * -1, 0);
+	}
+}
